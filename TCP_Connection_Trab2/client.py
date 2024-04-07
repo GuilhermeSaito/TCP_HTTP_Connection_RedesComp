6540b449fb366b88976b20fcc7916fc944a8da2a
GuilhermeSaito <guilhermetoshiosaito@gmail.com>
2024-04-07 01:29:47 -0300

    Added formato basico para realizar o trab2

---
+++ TCP_Connection_Trab2/client.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3  
+
+# Arrumar para colocar o checksum com o dado e a numeracao do pacote
+# Pensar em fazer outra forma de perda de pacote
+
+import socket
+import hashlib
+
+SERVER_IP = 'localhost'
+SERVER_PORT = 9999
+BUFFER = 2048
+
+def create_socket():
+    try:
+        global client_socket
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        return client_socket
+    
+    except socket.error as msg:
+        print("Socket creation error:" + str(msg))  
+
+def confere_checksum(dados, hash_received):
+    #Inicializa um objeto hashlib com o algoritmo SHA-256
+    hasher = hashlib.sha256()
+
+    #Atualiza o hasher com os dados de entrada
+    hasher.update(dados.encode('utf-8'))
+
+    #Calcula o checksum SHA-256 e retorna em formato hexadecimal
+    novo_checksum = hasher.hexdigest()
+
+    #retorna True se os checksum forem iguais e False, caso contrário
+    return hash_received == novo_checksum
+    
+def main():
+    # Inicializa o socket TCP
+    server_address = (SERVER_IP, SERVER_PORT)
+
+    socket = create_socket()
+    client_socket.connect(server_address)
+
+    recvFile = 'recvFile.txt'
+    backupFile = 'bckFile.txt'
+
+    print("Opções disponíveis:")
+    print("1. Sair")
+    print("2. Requisitar um arquivo")
+    print("3. Modo Chat")
+
+    while True:
+        choice = input("Escolha uma opção (1 ou 2 ou 3): ")
+        
+        # 1 = Sair
+        if choice == "1":
+            client_socket.send("Sair".encode('utf-8'))
+            break
+        # 2 = Requisitar o arquivo
+        elif choice == "2":
+            file_request = input("Digite o nome do arquivo desejado: ")
+            client_socket.send(f"Arquivo {file_request}".encode('utf-8'))
+        # 3 = Chat
+        elif choice == "3":
+            client_socket.send("Chat".encode('utf-8'))
+            print(client_socket.recv(1024).decode('utf-8'))
+            while True:
+                message = input("Digite sua mensagem (ou 'sair' para encerrar o chat): ")
+                if message.lower() == "sair":
+                    client_socket.send(message.encode('utf-8'))
+                    break
+                client_socket.send(message.encode('utf-8'))
+            print(client_socket.recv(1024).decode('utf-8'))
+        # QualquerOutraCoisa = Tente novamente
+        else:
+            print("Opcao invalida. Tente novamente")
+
+    print(client_socket.recv(1024).decode('utf-8'))
+    client_socket.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # package_count = 0
+
+    # arquivo = input('Insira o nome do arquivo + extensão \".txt\"\n')
+    # bytesEnviados = str.encode('GET ' + arquivo)
+
+    # # Envia a mensagem de solicitação para o servidor
+    # client_socket.sendto(bytesEnviados, server_address)
+
+    # try:
+    #     flag_error = 1
+        
+    #     # Recebe os dados do servidor em blocos e os imprime
+    #     with open(recvFile, 'wb') as recv_file, open (backupFile, 'wb') as bck_file:
+    #         while True:
+    #             package_received, _ = socket.recvfrom(BUFFER)
+
+    #             package_received_decoded = package_received.decode("utf-8")
+
+    #             hash_received = package_received_decoded[ :package_received_decoded.find(";")]
+    #             num_pack = package_received_decoded[package_received_decoded.find(";") + 1: package_received_decoded.find(">")]
+    #             data = package_received_decoded[package_received_decoded.find(">") + 1: ]
+
+    #             print("Num pack recebido: ")
+    #             print(num_pack)
+
+    #             if data == "1":
+    #                 print('Arquivo nao encontrado')
+    #                 flag_error = 0
+    #                 break
+    #             elif data.startswith('ERRO!'):
+    #                 print(data)
+    #                 flag_error = 0
+    #                 break
+    #             elif not data:
+    #                 print('Arquivo finalizado!')
+    #                 break  # Se não há mais dados, sai do loop
+                
+    #             bck_file.write(data.encode('utf-8'))
+
+    #             #Opção para o usuário descartar uma parte do arquivo (Simular perda de pacotes)
+    #             discard = input("Deseja descartar uma parte do arquivo? (s/n): ")
+    #             if discard.lower() == 's':
+    #                 percent_to_discard = float(input("Informe a porcentagem do arquivo a ser descartada (0-100): "))
+
+    #                 bytes_to_discard = int(len(data) * ((100 - percent_to_discard) / 100))
+    #                 data_dicard = data[:bytes_to_discard]
+    #                 data = data_dicard
+
+    #             # Verifica o checksum e solicita reenvio em caso de falha
+    #             if confere_checksum(data, hash_received):
+    #                 recv_file.write(data.encode('utf-8'))
+    #                 print(f'Pacote {package_count} enviado!')
+    #                 check = 'OK'.encode('utf-8')
+    #             else:
+    #                 print(f'Erro de checksum no pacote {package_count}. Uma parte do arquivo foi perdida.\nRequisitando reenvio.')
+    #                 check = 'NOK'.encode('utf-8')
+    
+    #             socket.sendto(check, server_address)
+    #         if flag_error:
+    #             print(f'Todos os dados recebidos do servidor escritos em: {backupFile}')
+    #             print(f'Somente os dados validos (excluido os dados que vieram faltando) {recvFile}')
+    
+    # except Exception as e:
+    #     print(f"Ocorreu um erro durante a comunicação com o servidor: {e}")
+    
+    # finally:
+    #     # Fecha o socket do cliente
+    #     print("Finalizado. Fechando conexão com o servidor.")
+    #     socket.close()
+
+if __name__ == "__main__":
+    main()
---
+++ TCP_Connection_Trab2/server.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3  
+import socket
+import hashlib
+import os
+import threading
+
+HOST = '0.0.0.0'
+PORT = 9999
+BUFFER = 1024
+
+#Create a Socket (connect two computers)
+def create_socket():
+    try:
+        global server_socket
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        return server_socket
+    
+    except socket.error as msg:
+        print("Socket creation error:" + str(msg))
+
+#Binding the socket and listening for connections
+def bind_socket(server_socket):
+    try:
+        print("Binding the Port: "+ str(PORT))
+
+        server_socket.bind((HOST, PORT))
+        server_socket.listen(5)
+
+    except socket.timeout as msg:
+        print("Socket Binding error" + str(msg) +"\n" + "Retrying...")
+        bind_socket(server_socket)
+
+def checksumSHA256(data):
+    #Inicializa um objeto hashlib com o algoritmo SHA-256
+    hasher = hashlib.sha256()
+
+    #Atualiza o hasher com os dados de entrada
+    hasher.update(data)
+
+    #Calcula o checksum SHA-256 e retorna em formato hexadecimal
+    checksum = hasher.hexdigest()
+    return checksum
+
+def request_file(client_socket, dataString):
+    print("Funcao para mandar os arquivos caso escolhido")
+
+def chat(client_socket):
+    print("Funcao para o servidor mandar exatamente igual o que estah recebendo para o cliente")
+
+#Recebe comandos do cliente
+def get_commands(client_socket, adress):
+    while True:
+        dataString = client_socket.recv(BUFFER).decode('utf-8').strip()
+
+        if dataString == 'Sair':
+            client_socket.send('Fechando conexão. '.encode('utf-8'))
+            client_socket.close()
+            break
+        
+        elif dataString.startswith('Arquivo'):
+            request_file(client_socket = client_socket, dataString = dataString)
+
+        elif dataString == 'Chat':
+            chat(client_socket = client_socket)
+    
+#Envia arquivo solicitado pelo cliente
+def send_file(fileName, adress, packetNumber):
+    packge_num = 0
+
+    if not os.path.isfile(fileName):
+        print("Arquivo nao encontrado: " + fileName)
+        server_socket.sendto(b"1", adress)
+    else:
+        try:
+            with open(fileName, 'rb') as f:
+                while data := f.read(BUFFER):
+                    packge_num += 1
+
+                    # Calcula checksum com SHA-256
+                    checksum = checksumSHA256(data)
+
+                    check = 'NOK'
+
+                    #Verifica se o pacote foi enviado corretamente
+                    while check == 'NOK':
+                        server_socket.sendto(checksum.encode('utf-8') + ";".encode('utf-8') + str(packge_num).encode('utf-8') + ">".encode('utf-8') + data, adress)
+                        check = server_socket.recvfrom(BUFFER)
+                        check = check[0].decode('utf-8')
+                        if check == 'NOK':
+                            print('NOK recebido. Reenviando parte do arquivo.')
+                    
+                    # Incrementa o número do pacote para o próximo
+                    # packetNumber += 1
+
+        #Se o arquivo não existir
+        except FileNotFoundError as msg:
+            print("Deu algum error:" + str(msg + "\n"))
+            server_socket.sendto(b"ERRO!", adress)
+            
+        # Mensagem de finalização do arquivo
+        print(f'Arquivo {fileName} enviado para {adress}')
+        server_socket.sendto(b'', adress)
+
+def main():
+    server_socket = create_socket()
+    bind_socket(server_socket)
+
+    print("Servidor está ouvindo em ", (HOST, PORT))
+
+    while True:
+        # Aqui recebe o ip, port
+        client_socket, adress = server_socket.accept()
+
+        print(f"Recebido conexao desse cliente: {adress[0]}, port: {adress[1]}")
+
+        client_thread = threading.Thread(target = get_commands, args = (client_socket, adress[1],))
+        client_thread.start()
+
+
+    # ################### Codigo para matar o processo que estah executando na porta especificada
+        # sudo kill -9 `sudo lsof -t -i:9999`
+        # Se nao der certo o comando de cima, usa esse:     sudo kill -9 $(sudo lsof -t -i:9999)
+    # ###################
+
+main()
